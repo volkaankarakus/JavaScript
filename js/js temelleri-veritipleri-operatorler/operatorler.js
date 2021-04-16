1cdacdde22bcd75354b2773075ec0a268c6659e1
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2021-04-16 04:02:54 +0300

    Add files via upload

---
+++ js/js temelleri-veritipleri-operatorler/operatorler.js
@@ -0,0 +1,21 @@
+// Aritmetik Operatorler 
+// + - * / %
+
+let sayi1=10
+let sayi2=30
+
+let sonuc= sayi1+sayi2
+console.log(sonuc)
+
+//arttirma ve azaltma operatorleri
+sayi1++
+sayi2--
+
+let yas=20
+let metinselYas='20'
+console.log(yas==metinselYas)//degerleri karsilastirir.
+console.log(yas===metinselYas)//hem degerlere hem de veri tipine bakar.
+
+//mantiksal operatorler
+console.log((sayi1<50) && (sayi2>20))
+console.log((sayi1<50) || (sayi2>20))
