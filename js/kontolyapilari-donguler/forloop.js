770b8e83ef9422c308f1b47ed34ab5018d822601
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2021-04-16 21:31:18 +0300

    Add files via upload

---
+++ js/kontolyapilari-donguler/forloop.js
@@ -0,0 +1,12 @@
+// for loop
+// for(ilk_atama;dongu_calistirma_sarti;her_calisma_sonrasi_ne_olacak)
+
+for(let i=0;i<20;i++){
+    console.log('20 kere yazdir.')
+}
+
+//sonsuz dongu olusturmak
+for(;;){
+
+}
+
