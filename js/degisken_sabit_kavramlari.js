f34e0a2c387324b5fdc845e24e665b33cbf58edc
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2021-04-16 03:09:46 +0300

    Add files via upload

---
+++ js/degisken_sabit_kavramlari.js
@@ -0,0 +1,9 @@
+// var, let, ve const ile degisken tanimlanabilir.
+
+let tcKimlik=123456
+const piSayisi=3.14 //program boyunca degistirilmesin istiyorsam const.
+let yasim =30
+
+console.log(yasim)
+
+
