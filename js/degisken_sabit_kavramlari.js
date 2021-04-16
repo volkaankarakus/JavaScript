bf689ecd893f4be84084818888ab037adaf75507
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2021-04-16 04:01:46 +0300

    Delete js directory

--- js/degisken_sabit_kavramlari.js
+++
@@ -1,9 +0,0 @@
-// var, let, ve const ile degisken tanimlanabilir.
-
-let tcKimlik=123456
-const piSayisi=3.14 //program boyunca degistirilmesin istiyorsam const.
-let yasim =30
-
-console.log(yasim)
-
-
