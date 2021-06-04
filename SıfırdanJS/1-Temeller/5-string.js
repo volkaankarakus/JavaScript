4e2f04246a702b0af934fc6d12254a700e38fadb
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2021-06-04 16:44:57 +0300

    Add files via upload

---
+++ SıfırdanJS/1-Temeller/5-string.js
@@ -0,0 +1,21 @@
+let value;
+
+const firstName="Louis";
+const lastName="Armstrong";
+
+const langs="Java,Python,C++";
+
+value=firstName + lastName;
+value=firstName + " " + lastName ;
+
+value=firstName.length;
+
+value=firstName.concat(" ",lastName," ","Caz");
+
+value=value.toLowerCase();
+value=value.toUpperCase();
+
+
+
+
+console.log(value);
