8e164dcb6ca6be7ce13b09a0503238754bd6fe7e
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2021-06-05 03:13:09 +0300

    Add files via upload

---
+++ SıfırdanJS/1-Temeller/2-Kosullar/1-kosullar.js
@@ -0,0 +1,17 @@
+// Karsilastirma Operatorleri
+
+// ==
+// ===
+// !=
+// !==
+// >
+// <
+// >=
+// <=
+
+console.log(2==2); //true
+console.log("js"=="java"); //false
+console.log(2=="2"); //string number karsilastirmasinda, number'i stringe cevirir. cevap : true.
+
+
+
