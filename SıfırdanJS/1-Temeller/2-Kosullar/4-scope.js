e09ebcb2ba4ded0f680d6a055be7177d27818ae8
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2021-06-06 22:59:53 +0300

    Add files via upload

---
+++ SıfırdanJS/1-Temeller/2-Kosullar/4-scope.js
@@ -0,0 +1,10 @@
+if(true){
+    var a=20;
+    let b=10;
+    const c=30;
+}
+console.log(a);
+console.log(b);
+console.log(c); // herhangi bir bolum icinde tanimlanmis degerlerden 
+                //  sadece a disariya tanimli sekilde cikar.
+                // var'in let ve const a gore avantajlarindan biri.
