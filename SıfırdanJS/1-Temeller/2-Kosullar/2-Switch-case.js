55481ffd78164ed6eb7b1653701208400b58265b
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2021-06-06 15:28:22 +0300

    Add files via upload

---
+++ SıfırdanJS/1-Temeller/2-Kosullar/2-Switch-case.js
@@ -0,0 +1,15 @@
+const process=2;
+
+switch (process){
+    case 1:
+        console.log("İslem 1");
+        break; // break yazmazsam digerlerine de girer
+    case 2:
+        console.log("İslem 2");
+        break;
+    case 3:
+        console.log("İslem 3");
+        break;
+    default:
+        console.log("Gecersiz İslem");
+}
