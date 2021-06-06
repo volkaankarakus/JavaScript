1ca2b6f9cd7ac4658c51ee2b247de8f249c7f415
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2021-06-06 15:22:44 +0300

    Update 1-kosullar.js

--- SıfırdanJS/1-Temeller/2-Kosullar/1-kosullar.js
+++ SıfırdanJS/1-Temeller/2-Kosullar/1-kosullar.js
@@ -12,6 +12,28 @@
 console.log(2==2); //true
 console.log("js"=="java"); //false
 console.log(2=="2"); //string number karsilastirmasinda, number'i stringe cevirir. cevap : true.
+console.log(2==="2"); // bu false doner.
 
+// MANTIKSAL BAGLACLAR
+// Not Operatoru
+console.log(!(2 == 2));
 
+// And Operatoru
+console.log((2==2) && ("Volkan"=="Volkan"));
 
+// Or Operatoru
+console.log((2==2) || ("Volkan"=="Volkan"));
+
+//////////////////////////////////////////////////////////////////////////
+const error=true;
+
+if(error==true){
+    console.log("Hata Olustu..");
+}
+else{
+    console.log("Hata Olusmadi..");
+}
+
+// Ternary Operator
+const number =120;
+console.log(number === 100 ? "Sayi 100dur" : "Sayi 100 degil.");
