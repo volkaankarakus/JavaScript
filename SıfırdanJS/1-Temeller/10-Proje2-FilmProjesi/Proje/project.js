3682d5caf6f9e76255738223b5e9df5b955446b6
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2021-06-28 01:01:44 +0300

    Update project.js

--- SıfırdanJS/1-Temeller/10-Proje2-FilmProjesi/Proje/project.js
+++ SıfırdanJS/1-Temeller/10-Proje2-FilmProjesi/Proje/project.js
@@ -13,17 +13,35 @@ const ui = new UI();
 // Storage Objesi Uretme 
 const storage = new Storage();
 
+// Filmi Sil butonu icin 2. card-body'yi secmemiz gerekiyor.
+const secondCardBody = document.querySelectorAll(".card-body")[1];
+
+// Butun Filmleri Silmek icin id'si "clear-films" olan butonu secelim
+const clear = document.getElementById("clear-films");
+
+
+
+
 // Tum eventleri yukleme 
 eventListeners();
 
 function eventListeners(){
     
     form.addEventListener("submit",addFilm);
+
+    // sayfa yuklendiginde filmler yuklensin
     document.addEventListener("DOMContentLoaded",function(){
+        // storage'dan getFilmsFromStorage'i cagirip ui'da loadAllFilms'e gonderelim
         let films = storage.getFilmsFromStorage();
         ui.loadAllFilms(films);
 
     });
+
+    // filmi sil icin 
+    secondCardBody.addEventListener("click",deleteFilm);
+
+    // butun filmleri sil icin
+    clear.addEventListener("click",clearAllFilms);
 }
 
 function addFilm(e){
@@ -57,4 +75,32 @@ function addFilm(e){
 
 
     e.preventDefault();
+}
+
+function deleteFilm(e){
+    console.log(e.target); // click'i Filmi Sil butonuna bastigimizda verdigi cikti :
+                            // <a href="#" id ="delete-film" class="btn btn-danger"> Filmi Sil </a>
+
+    // bu click'e basildiginda tum tr'yi kaldirmak istiyoruz
+    //   bunun icin de tr'den td'ye ; td'den de tr'ye gitmem gerekiyor( 2 kere parent element)
+
+    if(e.target.id ==="delete-film"){
+        ui.deleteFilmFromUI(e.target); 
+        
+        // simdi de filmi storage'dan silelim. Bunu filmTitle'ini baz alarak yapalim
+        storage.deleteFilmFromStorage(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
+        // console.log(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
+
+        ui.displayMessages("Silme islemi basarili...","success");
+    }
+}
+
+function clearAllFilms() {
+
+    if(confirm("Tum Filmleri Silmek İstediginize Emin misiniz ?")){
+        ui.clearAllFilmsFromUI();
+        storage.clearAllFilmsFromStorage();
+
+    }
+    
 }
