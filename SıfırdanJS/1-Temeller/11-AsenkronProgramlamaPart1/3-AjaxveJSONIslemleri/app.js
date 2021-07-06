f072c6e446eb84e4163abf8ef608d615cb6f12bc
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2021-07-06 17:22:59 +0300

    Update app.js

--- SıfırdanJS/1-Temeller/11-AsenkronProgramlamaPart1/3-AjaxveJSONIslemleri/app.js
+++ SıfırdanJS/1-Temeller/11-AsenkronProgramlamaPart1/3-AjaxveJSONIslemleri/app.js
@@ -10,6 +10,38 @@ function getAllEmployees(){
     //baglanti kurma 
     xhr.open("GET","employees.json",true);
 
+
+    // istegi gondermeden sonra, kod olarak oncesine load'u yaziyoruz
+    xhr.onload = function(){
+
+        //employees'i alalim
+        let list = document.getElementById("employees");
+
+
+        if(this.status == 200){
+            console.log(this.responseText); // json dosyasindaki butun veriler geldi.
+            console.log(typeof this.responseText); // string dondu.
+
+            //JSON array'e cevirmek icin
+            console.log(JSON.parse(this.responseText)); //suan veriler 3 indexli bir array icinde.
+            // bunlari tutmak icin de degisken tanimlayabiliriz
+            const employees = JSON.parse(this.responseText);
+
+            //simdi bu array'in icinde forEach ile gezinebiliriz
+            employees.forEach(function(emp){
+                // HTML'de tr icinde td'ler seklinde table'a veriler ekleyebiliyorduk
+                list.innerHTML += ` 
+                <tr>
+                    <td>${emp.name}</td>
+                    <td>${emp.department}</td>
+                    <td>${emp.salary}</td>
+                </tr>
+                `
+            });
+        }
+    }
+
     //istegi gonderme 
     xhr.send() // "get" request oldugu icin icine birsey vermiyoruz
+
 }
