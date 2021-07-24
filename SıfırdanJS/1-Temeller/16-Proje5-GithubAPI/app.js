fb594e672cfb077ba45863b4d7712e8cec6a7737
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2021-07-25 02:21:58 +0300

    Update app.js

--- SıfırdanJS/1-Temeller/16-Proje5-GithubAPI/app.js
+++ SıfırdanJS/1-Temeller/16-Proje5-GithubAPI/app.js
@@ -1,37 +1,63 @@
-// Element Secme 
-const githubForm = document.getElementById("github-form");
-const nameInput = document.getElementById("githubname");
+// butonlari ve formu secelim (Element Secme)
+const githubForm = document.getElementById('github-form');
+const nameInput = document.getElementById('githubname');
+const clearLastUsers = document.getElementById('clear-last-users');
 
-const clearLastUsers = document.getElementById("clear-last-users");
+const lastUsers = document.getElementById('last-users');
 
-// son aramalar icin ul
-const lastUsers  = document.getElementById("last-users");
+
+const github = new Github(); 
+
 
 eventListeners();
 
-function eventListeners () {
-    githubForm.addEventListener("submit",getData);
-    clearLastUsers.addEventListener("click",clearAllSearched);
-    document.addEventListener("DOMContentLoaded", getAllSearched); // son aramalari sayfa yenilendikce storage'dan alip arayuze yazicaz.
+function eventListeners(){
+    githubForm.addEventListener('submit',getData);
+    clearLastUsers.addEventListener('click',clearAllSearched);
+    document.addEventListener('DOMContentLoaded',getAllSearched); // son aramalari sayfa yenilendikce storagedan alip arayuze yazma 
+
 }
 
-function getData(e){
 
+function getData (e){
+    let username = nameInput.value.trim();
 
-
-
+    if( username === ""){
+        alert("Lutfen gecerli bir kullanici adi giriniz...");
+    }
+    else{
+        // request aticaz
+          // request atmak icin Github'dan obje olusturmamiz gerekiyor. yukarida olusturduk
+        github.getGithubData(username) // fonksiyon async olarak yazildigi icin ve burada bize obje donucegi icin bizim bunu Promise yapisiyla yakalamamiz gerekiyor
+        .then(response => {
+            if(response.user.message === "Not Found"){
+                // Hata Mesaji 
+                console.log("Hatali Username girdiniz....");
+            }
+            else{
+                console.log(response);
+            }
+        })
+        .catch(err => console.error(err));
+    
+    }
 
     
-    e.preventDefault()
+    
+    
+    e.preventDefault();
 }
 
+
+
 function clearAllSearched(){
-    // Tum Arananlari Temizle
+    // Tum arananlari temizle 
 
 }
 
 function getAllSearched(){
-    // Arananlari Storage'dan al ve UI'a ekle
+    // Arananlari storagedan UI'a ekle
+
 
 
 }
